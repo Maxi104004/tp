25dd8b4c139f3654b16a7b56de0b0bc5d23e15ca
Maxi104004 <107599256+Maxi104004@users.noreply.github.com>
2023-09-12 16:25:32 -0300

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class formRichtexBox
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formRichtexBox));
+            this.rchMiDoc = new System.Windows.Forms.RichTextBox();
+            this.btnTimes = new System.Windows.Forms.Button();
+            this.btnArial = new System.Windows.Forms.Button();
+            this.btnTam10 = new System.Windows.Forms.Button();
+            this.btnTam14 = new System.Windows.Forms.Button();
+            this.btnNegrita = new System.Windows.Forms.Button();
+            this.btnCursiva = new System.Windows.Forms.Button();
+            this.btnColorRojo = new System.Windows.Forms.Button();
+            this.btnColorAmarillo = new System.Windows.Forms.Button();
+            this.btnFonodoAzul = new System.Windows.Forms.Button();
+            this.btnFondoNaranja = new System.Windows.Forms.Button();
+            this.btnDefult = new System.Windows.Forms.Button();
+            this.btnNombre = new System.Windows.Forms.Label();
+            this.mTBDni = new System.Windows.Forms.MaskedTextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rchMiDoc
+            // 
+            this.rchMiDoc.Location = new System.Drawing.Point(12, 12);
+            this.rchMiDoc.Name = "rchMiDoc";
+            this.rchMiDoc.Size = new System.Drawing.Size(541, 209);
+            this.rchMiDoc.TabIndex = 1;
+            this.rchMiDoc.Text = "";
+            // 
+            // btnTimes
+            // 
+            this.btnTimes.Location = new System.Drawing.Point(568, 10);
+            this.btnTimes.Name = "btnTimes";
+            this.btnTimes.Size = new System.Drawing.Size(220, 23);
+            this.btnTimes.TabIndex = 2;
+            this.btnTimes.Text = "Time New Roman";
+            this.btnTimes.UseVisualStyleBackColor = true;
+            this.btnTimes.Click += new System.EventHandler(this.btnTimes_Click);
+            // 
+            // btnArial
+            // 
+            this.btnArial.Location = new System.Drawing.Point(568, 39);
+            this.btnArial.Name = "btnArial";
+            this.btnArial.Size = new System.Drawing.Size(220, 23);
+            this.btnArial.TabIndex = 3;
+            this.btnArial.Text = "Arial";
+            this.btnArial.UseVisualStyleBackColor = true;
+            this.btnArial.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnTam10
+            // 
+            this.btnTam10.Location = new System.Drawing.Point(568, 68);
+            this.btnTam10.Name = "btnTam10";
+            this.btnTam10.Size = new System.Drawing.Size(220, 23);
+            this.btnTam10.TabIndex = 4;
+            this.btnTam10.Text = "Tamaño 10";
+            this.btnTam10.UseVisualStyleBackColor = true;
+            this.btnTam10.Click += new System.EventHandler(this.btnTam10_Click);
+            // 
+            // btnTam14
+            // 
+            this.btnTam14.Location = new System.Drawing.Point(568, 97);
+            this.btnTam14.Name = "btnTam14";
+            this.btnTam14.Size = new System.Drawing.Size(220, 23);
+            this.btnTam14.TabIndex = 5;
+            this.btnTam14.Text = "Tamaño 14";
+            this.btnTam14.UseVisualStyleBackColor = true;
+            this.btnTam14.Click += new System.EventHandler(this.btnTam14_Click);
+            // 
+            // btnNegrita
+            // 
+            this.btnNegrita.Location = new System.Drawing.Point(568, 126);
+            this.btnNegrita.Name = "btnNegrita";
+            this.btnNegrita.Size = new System.Drawing.Size(220, 23);
+            this.btnNegrita.TabIndex = 6;
+            this.btnNegrita.Text = "Negrita";
+            this.btnNegrita.UseVisualStyleBackColor = true;
+            this.btnNegrita.Click += new System.EventHandler(this.btnNegrita_Click);
+            // 
+            // btnCursiva
+            // 
+            this.btnCursiva.Location = new System.Drawing.Point(568, 155);
+            this.btnCursiva.Name = "btnCursiva";
+            this.btnCursiva.Size = new System.Drawing.Size(220, 23);
+            this.btnCursiva.TabIndex = 7;
+            this.btnCursiva.Text = "Cursiva";
+            this.btnCursiva.UseVisualStyleBackColor = true;
+            this.btnCursiva.Click += new System.EventHandler(this.btnCursiva_Click);
+            // 
+            // btnColorRojo
+            // 
+            this.btnColorRojo.Location = new System.Drawing.Point(568, 184);
+            this.btnColorRojo.Name = "btnColorRojo";
+            this.btnColorRojo.Size = new System.Drawing.Size(220, 23);
+            this.btnColorRojo.TabIndex = 8;
+            this.btnColorRojo.Text = "Letra color Rojo";
+            this.btnColorRojo.UseVisualStyleBackColor = true;
+            this.btnColorRojo.Click += new System.EventHandler(this.btnColorRojo_Click);
+            // 
+            // btnColorAmarillo
+            // 
+            this.btnColorAmarillo.Location = new System.Drawing.Point(568, 213);
+            this.btnColorAmarillo.Name = "btnColorAmarillo";
+            this.btnColorAmarillo.Size = new System.Drawing.Size(220, 23);
+            this.btnColorAmarillo.TabIndex = 9;
+            this.btnColorAmarillo.Text = "Letra Color Amarillo";
+            this.btnColorAmarillo.UseVisualStyleBackColor = true;
+            this.btnColorAmarillo.Click += new System.EventHandler(this.btnColorAmarillo_Click);
+            // 
+            // btnFonodoAzul
+            // 
+            this.btnFonodoAzul.Location = new System.Drawing.Point(568, 246);
+            this.btnFonodoAzul.Name = "btnFonodoAzul";
+            this.btnFonodoAzul.Size = new System.Drawing.Size(220, 23);
+            this.btnFonodoAzul.TabIndex = 10;
+            this.btnFonodoAzul.Text = "Fondo Azul";
+            this.btnFonodoAzul.UseVisualStyleBackColor = true;
+            this.btnFonodoAzul.Click += new System.EventHandler(this.btnFonodoAzul_Click);
+            // 
+            // btnFondoNaranja
+            // 
+            this.btnFondoNaranja.Location = new System.Drawing.Point(568, 277);
+            this.btnFondoNaranja.Name = "btnFondoNaranja";
+            this.btnFondoNaranja.Size = new System.Drawing.Size(220, 23);
+            this.btnFondoNaranja.TabIndex = 11;
+            this.btnFondoNaranja.Text = "Fondo Naranja";
+            this.btnFondoNaranja.UseVisualStyleBackColor = true;
+            this.btnFondoNaranja.Click += new System.EventHandler(this.btnFondoNaranja_Click);
+            // 
+            // btnDefult
+            // 
+            this.btnDefult.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.btnDefult.Font = new System.Drawing.Font("MV Boli", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDefult.Location = new System.Drawing.Point(568, 312);
+            this.btnDefult.Name = "btnDefult";
+            this.btnDefult.Size = new System.Drawing.Size(220, 23);
+            this.btnDefult.TabIndex = 12;
+            this.btnDefult.Text = "MV Boli; 8,25pt";
+            this.btnDefult.UseVisualStyleBackColor = true;
+            this.btnDefult.Click += new System.EventHandler(this.btnDefult_Click);
+            // 
+            // btnNombre
+            // 
+            this.btnNombre.AutoSize = true;
+            this.btnNombre.Location = new System.Drawing.Point(13, 246);
+            this.btnNombre.Name = "btnNombre";
+            this.btnNombre.Size = new System.Drawing.Size(44, 13);
+            this.btnNombre.TabIndex = 13;
+            this.btnNombre.Text = "Nombre";
+            this.btnNombre.Click += new System.EventHandler(this.Nombre_Click);
+            // 
+            // mTBDni
+            // 
+            this.mTBDni.Location = new System.Drawing.Point(12, 314);
+            this.mTBDni.Name = "mTBDni";
+            this.mTBDni.Size = new System.Drawing.Size(216, 20);
+            this.mTBDni.TabIndex = 14;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(12, 264);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(216, 20);
+            this.txtNombre.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "label2";
+            // 
+            // formRichtexBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.mTBDni);
+            this.Controls.Add(this.btnNombre);
+            this.Controls.Add(this.btnDefult);
+            this.Controls.Add(this.btnFondoNaranja);
+            this.Controls.Add(this.btnFonodoAzul);
+            this.Controls.Add(this.btnColorAmarillo);
+            this.Controls.Add(this.btnColorRojo);
+            this.Controls.Add(this.btnCursiva);
+            this.Controls.Add(this.btnNegrita);
+            this.Controls.Add(this.btnTam14);
+            this.Controls.Add(this.btnTam10);
+            this.Controls.Add(this.btnArial);
+            this.Controls.Add(this.btnTimes);
+            this.Controls.Add(this.rchMiDoc);
+            this.Name = "formRichtexBox";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rchMiDoc;
+        private System.Windows.Forms.Button btnTimes;
+        private System.Windows.Forms.Button btnArial;
+        private System.Windows.Forms.Button btnTam10;
+        private System.Windows.Forms.Button btnTam14;
+        private System.Windows.Forms.Button btnNegrita;
+        private System.Windows.Forms.Button btnCursiva;
+        private System.Windows.Forms.Button btnColorRojo;
+        private System.Windows.Forms.Button btnColorAmarillo;
+        private System.Windows.Forms.Button btnFonodoAzul;
+        private System.Windows.Forms.Button btnFondoNaranja;
+        private System.Windows.Forms.Button btnDefult;
+        private System.Windows.Forms.Label btnNombre;
+        private System.Windows.Forms.MaskedTextBox mTBDni;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class formRichtexBox : Form
+    {
+        public formRichtexBox()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rchMiDoc.SelectionFont = new System.Drawing.Font("Arial", rchMiDoc.SelectionFont.Size, rchMiDoc.SelectionFont.Style);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Nombre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDefult_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTimes_Click(object sender, EventArgs e)
+        {
+            rchMiDoc.SelectionFont = new System.Drawing.Font("Time New Roman", rchMiDoc.SelectionFont.Size, rchMiDoc.SelectionFont.Style);
+        }
+
+        private void btnTam10_Click(object sender, EventArgs e)
+        {
+            rchMiDoc.SelectionFont = new Font (rchMiDoc.SelectionFont.FontFamily, 10, rchMiDoc.SelectionFont.Style);
+        }
+
+        private void btnTam14_Click(object sender, EventArgs e)
+        {
+            rchMiDoc.SelectionFont = new Font(rchMiDoc.SelectionFont.FontFamily, 14, rchMiDoc.SelectionFont.Style);
+        }
+
+        private void btnNegrita_Click(object sender, EventArgs e)
+        {
+            rchMiDoc.SelectionFont = new Font(rchMiDoc.SelectionFont.FontFamily, rchMiDoc.SelectionFont.Size, FontStyle.Bold);
+        }
+
+        private void btnCursiva_Click(object sender, EventArgs e)
+        {
+            rchMiDoc.SelectionFont = new Font(rchMiDoc.SelectionFont.FontFamily, rchMiDoc.SelectionFont.Size, FontStyle.Italic);
+        }
+
+        private void btnColorRojo_Click(object sender, EventArgs e)
+        {
+            rchMiDoc.SelectionColor=Color.Red;
+        }
+
+        private void btnColorAmarillo_Click(object sender, EventArgs e)
+        {
+            rchMiDoc.SelectionColor = Color.Yellow;
+        }
+
+        private void btnFonodoAzul_Click(object sender, EventArgs e)
+        {
+            rchMiDoc.SelectionBackColor=Color.Blue ;
+        }
+
+        private void btnFondoNaranja_Click(object sender, EventArgs e)
+        {
+            rchMiDoc.SelectionBackColor = Color.Orange;
+        }
+    }
+}
